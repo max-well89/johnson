b00108d6dab6e8204ee5ee48139652521eddf605
Max Well <m.volchkov@be-interactive.ru>
2018-07-08 23:23:45 +0300

    new

--- web/api/json.php
+++ web/api/json.php
@@ -3,7 +3,8 @@
 	require_once(dirname(__FILE__).'/../../api/lib/autoload.php');
     
 	try {
-        $context = new ApiContext(agContext::ENV_PROD);
+        //$context = new ApiContext(agContext::ENV_PROD);
+        $context = new ApiContext(agContext::ENV_DEBUG);
         $context->setUser(new agConfigUser($context));
 		$controller = new JsonApiController($context);
 		$context->setController($controller);
